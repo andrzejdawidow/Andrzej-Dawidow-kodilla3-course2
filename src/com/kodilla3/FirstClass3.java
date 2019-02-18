48c4b36535e9cb86985bbd4e81d6cd3f290577f0
andrzejdawidow <andrzejdawidow@gmail.com>
2019-02-18 21:17:57 +0100

    second

--- src/com/kodilla3/FirstClass3.java
+++ src/com/kodilla3/FirstClass3.java
@@ -3,5 +3,6 @@ package com.kodilla3;
 public class FirstClass3 {
     public static void main(String[] args) {
         System.out.println("witajcie ");
+        System.out.println("no i co???");
     }
 }
