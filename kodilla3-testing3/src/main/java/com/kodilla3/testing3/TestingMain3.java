5244a47a0477fe8bdda9b73742d51d82e71bb090
andrzejdawidow <andrzejdawidow@gmail.com>
2019-02-18 21:58:24 +0100

    fourth

---
+++ kodilla3-testing3/src/main/java/com/kodilla3/testing3/TestingMain3.java
@@ -0,0 +1,7 @@
+package com.kodilla3.testing3;
+
+public class TestingMain3 {
+    public static void main(String[] args){
+        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
