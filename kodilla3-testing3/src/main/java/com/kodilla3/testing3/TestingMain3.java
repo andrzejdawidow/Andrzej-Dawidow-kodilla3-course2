2c73e30775f92303417285a5eb27bed9df0dafdb
andrzejdawidow <andrzejdawidow@gmail.com>
2019-02-19 19:29:28 +0100

    dodałem pierwszy test

--- kodilla3-testing3/src/main/java/com/kodilla3/testing3/TestingMain3.java
+++ kodilla3-testing3/src/main/java/com/kodilla3/testing3/TestingMain3.java
@@ -1,7 +1,15 @@
 package com.kodilla3.testing3;
 
+import com.kodilla3.testing3.user.SimpleUser;
+
 public class TestingMain3 {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla3-testing3/src/main/java/com/kodilla3/testing3/user/SimpleUser.java
@@ -0,0 +1,11 @@
+package com.kodilla3.testing3.user;
+
+public class SimpleUser {
+    private String username;
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
