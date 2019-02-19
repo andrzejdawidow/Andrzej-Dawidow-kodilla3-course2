f723b652e723c4974ce25437f8995dde76e4ee63
andrzejdawidow <andrzejdawidow@gmail.com>
2019-02-19 21:08:20 +0100

    Zadanie: Pierwszy test jednostkowy

--- kodilla3-testing3/src/main/java/com/kodilla3/testing3/TestingMain3.java
+++ kodilla3-testing3/src/main/java/com/kodilla3/testing3/TestingMain3.java
@@ -7,14 +7,14 @@ public class TestingMain3 {
     public static void main(String[] args){
         //Test 1
         Calculator simpleCalculator = new Calculator();
-        int result = simpleCalculator.addAToB(5, 5);
+        int result = simpleCalculator.add(5, 5);
         if (result==(10)){
             System.out.println("add OK");
         } else {
             System.out.println("add Error!");
         }
         //Test2
-        int result1 = simpleCalculator.substractAFromB(5, 5);
+        int result1 = simpleCalculator.substract(5, 5);
         if (result1==(0)){
             System.out.println("substract OK");
         } else {
