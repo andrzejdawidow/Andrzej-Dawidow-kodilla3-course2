e8831b76c13dd2e02ea0e1ebe5689778804df5d7
andrzejdawidow <andrzejdawidow@gmail.com>
2019-03-16 18:58:21 +0100

    pogoda

---
+++ kodilla3-testing3/src/test/java/com/kodilla3/testing3/weather/stub/WeatherForecastTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla3.testing3.weather.stub;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WeatherForecastTestSuite {
+    @Test
+    public void testCalculateForecastWithStub() {
+//Given
+        Temperatures temperatures = new TemperaturesStub();
+        WeatherForecast weatherForecast = new WeatherForecast(temperatures);
+//When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+//Then
+        Assert.assertEquals(5, quantityOfSensors);
+    }
+}
