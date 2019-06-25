7d180b8530d117783179c7f8aa9dd9f9faefd1b1
andrzejdawidow <andrzejdawidow@gmail.com>
2019-06-25 18:24:40 +0200

    Referencje do metod 7_1

--- kodilla3-stream3/src/main/java/com/kodilla3/stream3/StreamMain.java
+++ kodilla3-stream3/src/main/java/com/kodilla3/stream3/StreamMain.java
@@ -4,13 +4,21 @@ import com.kodilla3.stream3.lambda.ExecuteSaySomething;
 import com.kodilla3.stream3.lambda.Executor;
 import com.kodilla3.stream3.lambda.ExpressionExecutor;
 import com.kodilla3.stream3.lambda.Processor;
+import com.kodilla3.stream3.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        System.out.println("Calculating expressions with lambdas");
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
     }
 }
